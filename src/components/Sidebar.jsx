6c0c02af4f0ffdcffc4b4f45e2abaa2fffd5547e
Pedro Henrique Almeida Lima <almeidaph13@gmail.com>
2022-11-22 11:36:15 -0300

    Add files via upload

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,27 @@
+import {PencilLine} from 'phosphor-react'
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://classic.exame.com/wp-content/uploads/2022/11/Selecao-Brasileira-2022.jpg?quality=50&strip=info&w=500"
+      />
+
+      <div className={styles.profile}>
+        <img className={styles.avatar} src="https://github.com/diego3g.png" />
+
+        <strong>Diego Fernandes</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+            <PencilLine size={20}/>
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
